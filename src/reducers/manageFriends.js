210bc58ee8e7ab84d44c179fbe4bc17a876e5c9d
claire <github email address>
2019-02-18 18:58:45 -0600

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,22 +1,43 @@
-export function manageFriends(state, action){
+
+export function manageFriends(state = {friends: []}, action){
     switch(action.type){
         case 'ADD_FRIEND': {
             return {
-                friends: [ { name: 'Avi', hometown: 'NYC', id: 100 }, {
-                    name: "Joe", 
-                    hometown: 'Boston', 
-                    id: 101
-                }]
-
+                ...state,
+                friends: [ ...state.friends, action.friend ]
             }
         }
         case 'REMOVE_FRIEND': {
-            return {
-                friends: [ { name: 'Avi', hometown: 'NYC', id: 100 }, { name: 'Steven', hometown: 'Philadephia', id: 102 } ] 
-            }
-        }
-
+            return {    
+                friends: Object.assign([], state.friends.filter(friend => friend.id != action.id))}}
         default:
             return state;
     }
 }
+
+
+
+
+// the following hard coded passes the Learn tests....
+// export function manageFriends(state, action){
+//     switch(action.type){
+//         case 'ADD_FRIEND': {
+//             return {
+//                 friends: [ { name: 'Avi', hometown: 'NYC', id: 100 }, {
+//                     name: "Joe", 
+//                     hometown: 'Boston', 
+//                     id: 101
+//                 }]
+
+//             }
+//         }
+//         case 'REMOVE_FRIEND': {
+//             return {
+//                 friends: [ { name: 'Avi', hometown: 'NYC', id: 100 }, { name: 'Steven', hometown: 'Philadephia', id: 102 } ] 
+//             }
+//         }
+
+//         default:
+//             return state;
+//     }
+// }
--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,7 +1,12 @@
+import { strictEqual } from "assert";
+
 export function managePresents(state, action){
     switch(action.type){
         case 'INCREASE': {
-            return {numberOfPresents: state.numberOfPresents + 1 }
+            return {
+                ...state, 
+                numberOfPresents: state.numberOfPresents + 1 
+            }
         }
         default:
             return state;
