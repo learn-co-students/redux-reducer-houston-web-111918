3e40c79c6e4636d397ba5eb059b9325112ca0197
rdaley321 <rdaley321@yahoo.com>
2019-02-20 11:13:30 -0600

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,13 @@
 export function manageFriends(state, action){
+  switch (action.type) {
+    case 'ADD_FRIEND':
+    console.log(action)
+      return {friends: [...state.friends, {name: action.friend.name, hometown: action.friend.hometown, id: action.friend.id}]};
+    break;
+    case 'REMOVE_FRIEND':
+      return {friends: state.friends.filter(friend => action.id !== friend.id)}
+    break;
+    default:
+      return state;
+  }
 }
